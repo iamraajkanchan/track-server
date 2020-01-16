a5d796502c8531562321aabfa450f9ff3cd98a4f
Raaj Kanchan <iamraajkanchan@gmail.com>
2020-01-16 22:56:07 +0530

    Project Completed

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoURI =
+  "mongodb+srv://mongoAdmin:rajmongoadminkanchan16@cluster0-dg5hz.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", err => {
+  console.log("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening to port 3000");
+});
